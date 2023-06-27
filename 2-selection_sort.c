a2d41b4133eadfed51dbeb644bee7a42bc5ec73f
perfecttinotenda <perfecttinotendamashingaidze@gmail.com>
2023-06-27 15:45:08 +0000

    Task 2

---
+++ 2-selection_sort.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+/**
+ * selection_sort - funkshen ino sorta an array
+ *@size:  zera re array
+ * @array: list yema nhamba
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, index;
+	int tmp, swap, flag = 0;
+
+	if (array == NULL)
+		return;
+	for (i = 0; i < size; i++)
+	{
+		tmp = i;
+		flag = 0;
+		for (index = i + 1; index < size; index++)
+		{
+			if (array[tmp] > array[index])
+			{
+				tmp = index;
+				flag += 1;
+			}
+		}
+		swap = array[i];
+		array[i] = array[tmp];
+		array[tmp] = swap;
+		if (flag != 0)
+			print_array(array, size);
+	}
+}
